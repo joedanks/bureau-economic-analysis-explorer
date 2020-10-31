79ee426e5dc3fabdc7a6d3343e02ba7aa47ab9ad
Levi Liester <levi.liester@gmail.com>
2020-10-30 23:57:08 -0500

    Pretty Table

--- src/Parameters.js
+++ src/Parameters.js
@@ -1,23 +1,58 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return {name, calories, fat, carbs, protein};
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 function Parameters(props) {
-    function buildParameter(parameter) {
-        return (
-            <div key={parameter.ParameterName}>
-                <label>{parameter.ParameterName}</label>
-                <span>{parameter.ParameterDescription}</span>
-                <label>Required: {parameter.ParameterIsRequiredFlag == 1 ? 'True' : 'False'}</label>
-            </div>
-        )
-    }
-    function buildParameters(parameters) {
-        return parameters.map(parameter => {
-            return buildParameter(parameter);
-        })
-    }
-    return (
-        <div>
-            {buildParameters(props.parameters)}
-        </div>
-    );
+  const classes = useStyles();
+  const parameters = props.parameters;
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Parameter Name</TableCell>
+            <TableCell align="right">Description</TableCell>
+            <TableCell align="right">Required Parameter</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {parameters.map((parameter) => (
+            <TableRow key={parameter.ParameterName}>
+              <TableCell component="th" scope="row">
+                {parameter.ParameterName}
+              </TableCell>
+              <TableCell align="right">{parameter.ParameterDescription}</TableCell>
+              <TableCell align="right">{parameter.ParameterIsRequiredFlag}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
 
 export default Parameters;
